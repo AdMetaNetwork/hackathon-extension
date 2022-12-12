3704ff5a32bb74ab56c68aaa9010febf8dcf6040
wxd56987 <672514143@qq.com>
2022-12-12 15:33:44 +0800

    update text

--- src/components/dashboard/nft.tsx
+++ src/components/dashboard/nft.tsx
@@ -7,7 +7,7 @@ const NFT: FC = () => {
 
   return (
     <div className="w-full pl-4 pr-4 mb-10">
-      <div className="text-white text-lg font-light italic mb-2">Your soul NFT is:</div>
+      <div className="text-white text-lg font-light italic mb-2">Your soul binding NFT is:</div>
       <div className="flex justify-start items-center">
         <Jazzicon diameter={40} seed={Math.round(Math.random() * 10000000)} />
         <div className="ml-4 flex justify-start items-center">
